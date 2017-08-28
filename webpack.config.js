2ee254fef3387e96f1c65395178c8dce327fc7c8
Raditya <arya.raditya26@gmail.com>
2017-08-28 22:30:22 +0700

    added path for changing output location

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,15 @@
+const path = require('path');
+
 module.exports={
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.join(__dirname, './build')
   }
 }
+
+/**
+ *  NOTE :
+ *  => path.join() documentation : https://nodejs.org/api/path.html#path_path_join_paths
+ *
+ */
